fa7656a58895866bade0ad75b179d8399e8bd4e8
Sebastian Simarro <ssebastian210@gmail.com>
2022-01-15 13:39:14 -0300

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,105 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../globals.css";
+import type { AppProps } from "next/app";
+import theme from "../theme";
+import {
+  ChakraProvider,
+  Stack,
+  Container,
+  Box,
+  Heading,
+  Text,
+  Flex,
+  Image,
+  Link,
+  Divider,
+} from "@chakra-ui/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Container
+        backgroundColor="white"
+        borderRadius="sm"
+        maxWidth="container.xl"
+        padding={4}
+      >
+        <Stack spacing={8}>
+          <Stack marginBottom={4} spacing={4}>
+            {/* <Image
+              borderRadius="lg"
+              height="100%"
+              maxHeight={64}
+              objectFit="cover"
+              src={INFORMATION.banner}
+            /> */}
+            <Stack
+              alignItems="center"
+              direction={{ base: "column", sm: "row" }}
+              spacing={{ base: 3, sm: 6 }}
+            >
+              <Box
+                backgroundColor="white"
+                borderRadius={9999}
+                marginTop={{ base: -12, sm: -16 }}
+                minWidth={{ base: 24, sm: 32 }}
+                padding={1}
+              >
+                {/* <Image
+                  borderRadius={9999}
+                  height={{ base: 24, sm: 32 }}
+                  src={INFORMATION.avatar}
+                  width={{ base: 24, sm: 32 }}
+                /> */}
+              </Box>
+              <Stack
+                alignItems={{ base: "center", sm: "flex-start" }}
+                spacing={3}
+                textAlign={{ base: "center", sm: "left" }}
+              >
+                <Stack spacing={0}>
+                  <Heading>titulo</Heading>
+                  <Text color="gray.500" fontWeight="500">
+                    Description
+                  </Text>
+                </Stack>
+                {/* <Stack direction="row">
+                  {INFORMATION.social.map((social) => (
+                    <Link key={social.name} isExternal href={social.url}>
+                      <Flex
+                        alignItems="center"
+                        backgroundColor="primary.500"
+                        borderRadius={9999}
+                        color="white"
+                        height={10}
+                        justifyContent="center"
+                        width={10}
+                      >
+                        <Image
+                          src={`https://icongr.am/fontawesome/${social.name}.svg?size=24&color=ffffff`}
+                        />
+                      </Flex>
+                    </Link>
+                  ))}
+                </Stack> */}
+              </Stack>
+            </Stack>
+          </Stack>
+          <Component {...pageProps} />
+        </Stack>
+        <Divider marginY={4} />
+        {/* Inicio de copyright - Cambiar el contenido de los mismos viola el contenido de los terminos de licencia */}
+        {/* <Text textAlign="center">
+          © Copyright {new Date().getFullYear()}. Hecho con ♥ para la comunidad,
+          por{" "}
+          <Link isExternal href="https://gonzalopozzo.com">
+            goncy
+          </Link>
+          .
+        </Text> */}
+        {/* Fin de copyright */}
+      </Container>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default App;
